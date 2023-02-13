39c92bacc039bd576fe69c8bc04354de396c73b6
MrEpoch <109413478+MrEpoch@users.noreply.github.com>
2023-02-13 20:18:17 +0100

    save of current state i will think about that if i add button or use other things, i am heavily inspired by one work in solutions on odin project i still read documentation of how to get all information from weather api, what i get is incomplete, i leave here in fetch other API, faker api which i like because it doesn't need API key

--- src/app.js
+++ src/app.js
@@ -3,18 +3,13 @@ import * as htmlMe from "./components/methods";
 
 // faker api:https://fakerapi.it/api/v1/credit_cards?_quantity=1
 async function weatherLoad(location) {
-    const fetchWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3e2ec8741165eedcbeb5503d51b9acfc`, { mode: "cors" })
+    const fetchWeather = await fetch(`https://fakerapi.it/api/v1/credit_cards?_quantity=1`, { mode: "cors" })
     const weather = await fetchWeather.json();
     return weather;
 } 
 
 let location = "London";
 let Myweather = weatherLoad(location)
-
-const inputTaker = () => {
-    
-}
-
 
 const extraInfo = (weather) => {
     const leftContainer = htmlMe.divCreate("", "left-info")
@@ -30,7 +25,8 @@ const middleSearch = () => {
 }
 
 const mainInfo = (weather) => {
-    const rightContainer = htmlMe.divCreate("", "right-info")
+    const rightContainer = htmlMe.divCreate("", "right-info");
+    const place = htmlMe.h3Create();
     return rightContainer;
 }
 
@@ -44,6 +40,11 @@ const areaLoad = (arrToAppend, areaClass) => {
 
 const top = areaLoad([mainInfo(Myweather), middleSearch(), extraInfo(Myweather)], "top");
 const bottom = areaLoad([], "bottom");
+const search = top.getElementsByClassName("search");
 
+search.addEventListener("enter", () => {
+    location = search.value;
+
+})
 
 document.body.append(top, bottom)
